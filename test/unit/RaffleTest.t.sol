8f72993721904cce58e521e453519880c2e5fb11
tmpITK <torok.mark.patrik@gmail.com>
2023-07-11 16:21:22 +0200

    Add test file

---
+++ test/unit/RaffleTest.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {Raffle} from "../../src/Raffle.sol";
+import {DeployRaffle} from "../../script/DeployRaffle.s.sol";
+import {console} from "forge-std/console.sol";
+
+contract RaffleTest is Test {}
