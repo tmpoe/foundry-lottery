7b6bf22e25e3dd125b38defc6f53068ee5da4db7
tmpITK <torok.mark.patrik@gmail.com>
2023-07-11 16:18:11 +0200

    Add actual sub id to sepolia chainlink

--- script/Config.s.sol
+++ script/Config.s.sol
@@ -30,7 +30,7 @@ contract Config is Script {
             NetworkConfig({
                 entranceFee: 0.01 ether,
                 vrfCoordinatorAddress: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
-                subscriptionId: 0,
+                subscriptionId: 415,
                 callbackGasLimit: 500000,
                 gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
                 lengthOfRaffle: 30
